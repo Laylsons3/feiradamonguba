6fc672d871338f8d0b337846221f47b9d4e1b399
Laylson Sousa <laylsons3@gmail.com>
2024-03-31 16:11:23 -0300

    Adicionando todos os dados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,161 @@
+import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
+  const images = [
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24,
+    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 40, 41, 42, 43, 44, 45, 46, 48,
+    49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+  ];
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header className="w-full h-16 bg-zinc-200 absolute">
+        <div className="flex lg:flex-row flex-col items-center justify-between py-2 px-4 h-full">
+          <h1 className="flex items-center text-xl font-bold">
+            Feira de Artesanato da Monguba
+          </h1>
+          <nav className="flex gap-x-4 items-center">
+            <a className="hover:underline" href="#section-1">
+              Início
+            </a>
+            <a className="hover:underline" href="#section-2">
+              Vídeo
+            </a>
+            <a className="hover:underline" href="#section-3">
+              Fotos
+            </a>
+            <a className="hover:underline" href="#section-4">
+              Sobre
+            </a>
+            <a className="hover:underline" href="#section-5">
+              Contato
+            </a>
+          </nav>
         </div>
-      </div>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+      <main>
+        <section
+          id="section-1"
+          className="h-screen px-4 bg-slate-800 text-zinc-100 gap-y-4 flex w-full flex-col justify-center items-center pt-16"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <Image
+            src="/geral.jpeg"
+            width={600}
+            height={400}
+            alt="Foto geral da feira"
+          />
+          <p className="max-w-xl text-justify">
+            A Feira de Artesanato da Monguba foi realizada no dia 08 de março de
+            2024 (Dia Internacional da Mulher) na Escola Major Manoel Assis
+            Nepomuceno na Monguba, Pacatuba-CE.
           </p>
-        </a>
+          <p className="max-w-xl text-justify">
+            Participaram da feira artesãs da própria comunidade e puderam
+            mostrar seus trabalhos com diversos tipos diferentes de artesanato
+            como: Cerâmica, Pintura em telha, Crochê e EVA.
+          </p>
+        </section>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <section
+          id="section-2"
+          className="px-4 h-screen bg-cyan-800 text-zinc-100 gap-y-4 flex w-full flex-col justify-center items-center"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <h1 className="text-xl font-bold">Mini Documentário</h1>
+          <iframe
+            className=" lg:w-[860px] h-[480px]"
+            src="https://www.youtube-nocookie.com/embed/K9hLlgNOc3Q?si=Y1LRl1BcaTSyWLOR"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          />
+          <p className="max-w-[860px] text-justify">
+            Durante a feira foi feita filmagens para produção deste pequeno
+            vídeo, mostrando um pouco do trabalho de cada uma das artesãs.
           </p>
-        </a>
+        </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <section
+          id="section-3"
+          className="h-full bg-slate-800 text-zinc-100 gap-y-4 flex w-full flex-col justify-center items-center py-4"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <h1 className="text-xl font-bold">Registro fotográfico da feira</h1>
+          <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+            {images.map((image) => (
+              <Image
+                key={image}
+                alt="foto-feira"
+                width={200}
+                height={200}
+                src={`/fotos/feira (${image}).jpeg`}
+              />
+            ))}
+          </div>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <section
+          id="section-4"
+          className="px-4 h-screen bg-cyan-800 text-zinc-100 gap-y-4 flex w-full flex-col justify-center items-center"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <h1 className="text-xl font-bold">Sobre</h1>
+          <div className="max-w-[860px] flex flex-col gap-y-4 text-justify">
+            <p>
+              O projeto &quot; &ldquo; &#34; &rdquo; Artesanato - Mãos
+              Talentosas é motivado pela necessidade de preservar e promover o
+              patrimônio cultural e artístico do bairro da Monguba e da tribo
+              Pitaguary, enquanto destaca a criatividade e habilidade dos
+              artesãos locais. O artesanato desempenha um papel fundamental na
+              preservação da identidade cultural dessas comunidades, sendo uma
+              fonte de renda e um meio de expressão artística. As filmagens
+              foram feitas no dia 8 de março de 2024 na Escola Major Assis
+              Nepomuceno em Monguba.
+            </p>
+            <p>
+              Projeto: Artesanato Mãos Talentosas; Número do TEC Termo de
+              Execução Cultural: 0052/2024; Município de Pacatuba-CE; Secretaria
+              de Cultura
+            </p>
+            <p>
+              Artesãs: Cícera Feitosa Antônia Maria Feitosa Maria Madalena
+              Francisca Sousa Direção: Alex Gomes Coordenação: Laylson Sousa
+              Audiovisual: Henan Baltazar
+            </p>
+          </div>
+        </section>
+
+        <section
+          id="section-5"
+          className="h-screen bg-slate-800 text-zinc-100 gap-y-4 flex w-full flex-col justify-center items-center py-4"
+        >
+          <h1 className="text-xl font-bold">Contatos</h1>
+          <div className="flex flex-col">
+            <table className="w-bg-zinc-200">
+              <tbody>
+                <tr>
+                  <td className="px-1 lg:px-4 py-2 ">Direção:</td>
+                  <td className="px-1 lg:px-4 py-2 ">Alex Gomes</td>
+                  <td className="px-1 lg:px-4 py-2 ">(85) 9 8799-4777</td>
+                </tr>
+                <tr>
+                  <td className="px-1 lg:px-4 py-2 ">Coordenação:</td>
+                  <td className="px-1 lg:px-4 py-2 ">Laylson Sousa</td>
+                  <td className="px-1 lg:px-4 py-2 ">(85) 9 9644-5801</td>
+                </tr>
+                <tr>
+                  <td className="px-1 lg:px-4 py-2 ">Audiovisual:</td>
+                  <td className="px-1 lg:px-4 py-2 ">Henan Baltazar</td>
+                  <td className="px-1 lg:px-4 py-2 ">(85) 9 8833-3854</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </section>
+      </main>
+
+      <footer className="h-16 bg-zinc-900 text-white flex items-center justify-center">
+        <p>Feira de Artesanato da Monguba &copy; 2024</p>
+      </footer>
+    </div>
   );
 }
